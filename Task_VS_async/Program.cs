73fbd6e1ec2d8007e65cf8ed73c5397d2a98c217
Артём Журавлёв <artembestnet@mail.ru>
2019-03-21 09:15:28 +0300

    Добавьте файлы проекта.

---
+++ Task_VS_async/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_VS_async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
